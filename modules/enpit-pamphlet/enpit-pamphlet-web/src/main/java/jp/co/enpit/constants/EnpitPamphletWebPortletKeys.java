1d5f0eb218bc1c646e8ea1b5a3166ee2c9367103
Yasuyuki Takeo <yasuyuki.takeo@liferay.com>
2019-08-21 10:22:19 +0900

    mvc portlet added

---
+++ modules/enpit-pamphlet/enpit-pamphlet-web/src/main/java/jp/co/enpit/constants/EnpitPamphletWebPortletKeys.java
@@ -0,0 +1,11 @@
+package jp.co.enpit.constants;
+
+/**
+ * @author yasuflatland
+ */
+public class EnpitPamphletWebPortletKeys {
+
+	public static final String ENPITPAMPHLETWEB =
+		"jp_co_enpit_EnpitPamphletWebPortlet";
+
+}
